e9c13bcd7d3925c012bbb4373bb64e0cc62c5ef8
BezdanArpad <arpad.bezdan@optinkft.hu>
2020-04-27 11:55:40 +0200

    Uart tester added

---
+++ UartTeszter/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace UartTeszter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.port_name = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(56, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Port";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(676, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(767, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // port_name
+            // 
+            this.port_name.AutoSize = true;
+            this.port_name.Location = new System.Drawing.Point(24, 84);
+            this.port_name.Name = "port_name";
+            this.port_name.Size = new System.Drawing.Size(26, 13);
+            this.port_name.TabIndex = 5;
+            this.port_name.Text = "Port";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 8000000;
+            this.serialPort1.ReadBufferSize = 214748362;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(858, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 25);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Receive";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(56, 84);
+            this.richTextBox1.MaxLength = 4095;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1473, 530);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(949, 47);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1550, 626);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.port_name);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label port_name;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ UartTeszter/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace UartTeszter
+{
+    public partial class Form1 : Form
+    {
+
+        private static System.Timers.Timer aTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string[] ports = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+            comboBox1.SelectedIndex = 0;
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+
+            try
+            {
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.Open();
+            } catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int readed_something = 0;
+                int frame_numbers = 0;
+                if (serialPort1.IsOpen)
+                {
+
+
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+
+
+
+                    String value = serialPort1.ReadExisting();
+                    int readed_values = 0;
+                    Console.WriteLine("String length :" + value.Length);
+
+                    while (readed_something == 0)
+                    {
+                        value = serialPort1.ReadExisting();
+                        readed_values += value.Length;
+
+                        frame_numbers = readed_values / 5769;
+                        while (!value.Equals(""))
+                        {
+                            value += serialPort1.ReadExisting();
+                            readed_values += value.Length;
+                            frame_numbers = readed_values / 5769;
+                            readed_something = 1;
+                            if (sw.ElapsedMilliseconds > 5000)
+                            {
+                                break;
+                            }
+                            //richTextBox1.AppendText(value);
+                        }
+                    }
+
+                    //richTextBox1.Text = value;
+
+
+                    string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                    using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt")))
+                    {
+                            outputFile.WriteLine(value);
+                    }
+
+                    Console.WriteLine("Frame numbers : " + frame_numbers);
+                    Console.WriteLine("Data : " + readed_values);
+                }
+
+            } catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                Console.WriteLine("Closed");
+                button1.Enabled = true;
+                button2.Enabled = false;
+            } else
+            {
+                Console.WriteLine("Not Opened");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+    }
+}
